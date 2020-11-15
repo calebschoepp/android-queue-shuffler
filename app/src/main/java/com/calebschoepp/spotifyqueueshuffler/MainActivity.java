756d03937b319166d98ea561bb2e01b204d334b5
Caleb Schoepp <caleb.schoepp@gmail.com>
2020-11-15 00:37:19 -0700

    Add logging for debugging purposes

--- app/src/main/java/com/calebschoepp/spotifyqueueshuffler/MainActivity.java
+++ app/src/main/java/com/calebschoepp/spotifyqueueshuffler/MainActivity.java
@@ -19,6 +19,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     private static final String CLIENT_ID = "a13b7f5ec1c7478f92ce6eb811323012";
     private static final String REDIRECT_URI = "com.calebschoepp.spotifyqueueshuffler://callback";
     private SpotifyAppRemote mSpotifyAppRemote;
@@ -50,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
                     public void onConnected(SpotifyAppRemote spotifyAppRemote) {
                         mSpotifyAppRemote = spotifyAppRemote;
-                        mPlayer = new SyncPlayerApi(mSpotifyAppRemote.getPlayerApi(), 500);
+                        mPlayer = new SyncPlayerApi(mSpotifyAppRemote.getPlayerApi(), 1000);
                     }
 
                     public void onFailure(Throwable throwable) {
@@ -81,18 +82,24 @@ public class MainActivity extends AppCompatActivity {
 
                 // Pause the player
                 mPlayer.pause();
+                Log.d(TAG, "Pause");
 
                 // Add sentinel song to queue
                 mPlayer.queue(mSentinelTrackUri);
+                Log.d(TAG, "Queue sentinel");
 
                 // Log and skip songs until sentinel song is found
                 List<String> queuedSongs = new ArrayList<>();
                 while (true) {
                     mPlayer.skipNext();
+                    Log.d(TAG, "Skip next");
                     mPlayer.pause();
+                    Log.d(TAG, "Pause");
                     playerState = mPlayer.getPlayerState();
+                    Log.d(TAG, "Get player state");
 
                     // Found the sentinel song
+                    Log.d(TAG, String.format("Found sentinel: %b", playerState.track.uri.equals(mSentinelTrackUri)));
                     if (playerState.track.uri.equals(mSentinelTrackUri)) {
                         break;
                     }
@@ -105,15 +112,20 @@ public class MainActivity extends AppCompatActivity {
 
                 // Reset player
                 mPlayer.queue(currentSongUri);
+                Log.d(TAG, "Queue current song");
                 mPlayer.skipNext();
+                Log.d(TAG, "Skip next");
                 mPlayer.seekTo(currentSongPosition);
+                Log.d(TAG, "Seek position");
                 if (currentSongIsPaused) {
                     mPlayer.pause();
+                    Log.d(TAG, "Reause");
                 }
 
                 // Add shuffled songs to the queue
                 for (String uri : queuedSongs) {
                     mPlayer.queue(uri);
+                    Log.d(TAG, String.format("Queue %s", uri));
                 }
 
             } catch (Exception e) {
